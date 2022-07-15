bcdf82e53246436507abbaed7a103d95e35fa282
andresguti19fc <chavogutierrez19@gmail.com>
2022-07-15 19:44:25 -0300

    s

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="py-4 bg-dark fixed-bottom">
       <div className="container">
         <p className="m-0 text-center text-white">
-          Copyright &copy; AdictaSomos 2022.
+          Copyright &copy; AdictaSomos 2022. All rights reserved
         </p>
       </div>
     </footer>
